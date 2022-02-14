9c78e571dc09c7bcd3b92c63cd6b7d91704d9293
venzel <eneas.eng@yahoo.com>
2022-02-14 14:05:55 -0300

    feat: setup inicial

---
+++ src/components/Citacao.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- <div class="citacao">{{ p_citacao }}</div> -->
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: {
+    //     p_citacao: {
+    //         type: String,
+    //     },
+    // },
+};
+</script>
+
+<style scoped>
+.citacao {
+    border: 1px solid #ddd;
+    background-color: rgba(0, 0, 0, 0.1);
+    padding: 25px;
+    max-width: 450px;
+}
+</style>
---
+++ src/components/Citacoes.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+
+        <!-- <Citacao :p_citacao="alterar()" /> -->
+
+        <Citacao>
+            <p slot="autor">{{ citacoes[indice].autor }}</p>
+            <p slot="texto">{{ citacoes[indice].texto }}</p>
+            <p slot="fonte">{{ citacoes[indice].fonte }}</p>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao.vue';
+export default {
+    components: { Citacao },
+
+    data() {
+        return {
+            numero: 0,
+            citacoes: [
+                {
+                    fonte: 'Jornal do Empreendedor',
+                    texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                    autor: 'Margaret Mead',
+                },
+                {
+                    fonte: 'Frases de Mãe',
+                    texto: 'Isso não é um quarto, é um chiqueiro.',
+                    autor: 'Roberta',
+                },
+                {
+                    fonte: 'Frases de Pai',
+                    texto: 'Vou contar até 3! 1, 2, 2...',
+                    autor: 'Marcos',
+                },
+            ],
+        };
+    },
+    methods: {
+        alterar() {
+            return this.citacoes[this.indice].texto;
+        },
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.citacoes {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
